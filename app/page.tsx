8c489293424436f6c1aa02d99e6543050ae12c51
Vinay Kumar <n4ryn@Vinays-MacBook-Air.local>
2026-07-14 18:34:57 +0530

    feat: add theme & query provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
 export default function Home() {
-  return <h1>hello</h1>;
+  return (
+    <div>
+      <h1>hello</h1>
+      <ModeToggle />
+    </div>
+  );
 }
